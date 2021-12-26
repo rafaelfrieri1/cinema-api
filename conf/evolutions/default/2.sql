7d1fa6f9a79f305e5a0f4d6bc0b737eec5ebe53e
Rafael Frieri <r.frieri@javeriana.edu.co>
2021-12-26 15:34:16 -0500

    Added table to save showtimes and prices of movies.

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,21 @@
+# --- !Ups
+
+CREATE TABLE `cinema`.`movie_show_details`(
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `movie_id` INT(11) NOT NULL,
+  `show_time` TIMESTAMP NOT NULL,
+  `price` DECIMAL(7,2),
+  `active` TINYINT(1) NOT NULL DEFAULT 1,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  CONSTRAINT `movie_id_fk`
+    FOREIGN KEY (`movie_id`)
+    REFERENCES `cinema`.`movies` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+# --- !Downs
+
+DROP TABLE `cinema`.`movie_show_details`;
